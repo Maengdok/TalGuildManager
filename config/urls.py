1e79ab8e05993b299220606ef7b7258b489a9203
Maengdok <maengdok@outlook.com>
2024-10-24 18:47:33 +0200

    feat: added grade model

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('src.member.urls'), name="member_"),
+    path('grade/', include('src.grade.urls'), name="grade_"),
 ]
