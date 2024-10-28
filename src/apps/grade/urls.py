08b5de62aa6d2c2872f43b9e6cffdcc391b8365b
Maengdok <maengdok@outlook.com>
2024-10-28 16:41:50 +0100

    feat: created grades api routes

--- src/apps/grade/urls.py
+++ src/apps/grade/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import list 
+from .views import list, show, add, update, deactivate, delete
 
 urlpatterns = [
     path('', list, name='list'),
+    path('<int:grade_id>', show, name='show'),
+    path('add', add, name='add'),
+    path('update', update, name='update'),
+    path('deactivate/<int:grade_id>', deactivate, name='deactivate'),
+    path('delete/<int:grade_id>', delete, name='delete'),
 ]
